dd0bfd03be57c648215a3dacb458bb0819ffaeab
Jim Flanagan <flanagan.james@gmail.com>
2014-01-04 14:15:26 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WordsOnRails::Application.config.secret_key_base = 'e788be2b238b6313594448eca1bb5fb73ed62b9d7ec9b64c38306ff38cc0388509248c5d32dd5bbff8adad385e5533dc3188da7827a2e151f49615eeea28a0f4'
