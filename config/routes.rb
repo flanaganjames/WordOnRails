fd7d8620d975a059d58f5bbdb8b3a40df5b8d2c8
Jim Flanagan <flanagan.james@gmail.com>
2014-01-04 18:26:34 -0700

    back to basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 WordsOnRails::Application.routes.draw do
+  get "static/home"
   resources :users
-  match '/', to: '#index', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
