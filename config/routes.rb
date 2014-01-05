9bca4810a88fa41438eb9d3db3f97a6548fb0092
Jim Flanagan <flanagan.james@gmail.com>
2014-01-04 18:21:32 -0700

    copied erb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WordsOnRails::Application.routes.draw do
   resources :users
+  match '/', to: '#index', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
